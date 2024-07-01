30b2963ea5fc156c2ef71eb3c39b9db833cc3c49
Jonathan Victor <jhonathanvictor142@gmail.com>
2024-07-01 16:26:50 -0300

    Animação de opacidade na lista detalhes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const btnExpandir = document.querySelector('.menu-icone-expandir');
 const txtLink = document.querySelectorAll('.txt-link');
 const secao = document.querySelectorAll('.secao');
 const listaDeItemMenu = document.querySelectorAll('.lista-item');
+const ListaDecontainersDetalhes = document.querySelectorAll('.container-detalhes');
 
 // Expande o menu lateral
 btnExpandir.addEventListener('click', () => {
@@ -12,4 +13,22 @@ btnExpandir.addEventListener('click', () => {
     });
 
     btnExpandir.classList.toggle('invertido-vertical');
+});
+
+// Faz a inclusão de opacidade nos elementos que não foram clicados na lista de detalhes.
+ListaDecontainersDetalhes.forEach(container => {
+    container.addEventListener('click', (event) => {
+        event.stopPropagation(); // Impede que o clique se propague para o documento
+        ListaDecontainersDetalhes.forEach(item => {
+            item.classList.add('container-detalhes-opacidade');
+        });
+        container.classList.remove('container-detalhes-opacidade');
+    });
+});
+
+//
+document.addEventListener('click', () => {
+    ListaDecontainersDetalhes.forEach(item => {
+        item.classList.remove('container-detalhes-opacidade');
+    });
 });
