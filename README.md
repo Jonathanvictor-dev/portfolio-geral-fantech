2c5c1c65752eba63530f03a6a3fd814f88b6dbb7
Jonathan Victor <jhonathanvictor142@gmail.com>
2024-07-06 16:05:47 -0300

    readme inserido e ajuste no layout footer

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <img src="./img/fantechlogo.png" alt="Imagem do site, tela de início" width="150">
+</p>
+
+# Alunos - Técnico em Programação | Portfólio Geral
+### Direcionamento deploy: [Portfólio Geral](https://portfolio-geral-fantech.vercel.app/)
+### Seja direcionado para FAN.tech: [FAN.tech](https://fan-edu.com.br/fantech/)
+
+## Sobre 
+#### O portfólio da turma é destinado à divulgação dos projetos desenvolvidos na disciplina de Front-end. Aqui, você encontrará os projetos individuais dos alunos da turma de Técnico em Programação no período matutino
+
+<p align="center">
+  <img src="./img/portfolioGeralInicio.PNG" alt="Imagem do site, tela de início">
+</p>
+<p align="center">
+  <img src="./img/portfolioGeralSobre.PNG" alt="Imagem do site, tela de sobre">
+</p>
+<p align="center">
+  <img src="./img/portfolioGeralProjetos.PNG" alt="Imagem do site, tela de Projetos">
+</p>
+
+<hr style="margim-top: 0.5rem">
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const containerMenu = document.querySelector('.container-menu');
 const btnExpandir = document.querySelector('.menu-icone-expandir');
 const txtLink = document.querySelectorAll('.txt-link');
-const secao = document.querySelectorAll('.secao');
-const listaDeItemMenu = document.querySelectorAll('.lista-item');
 const ListaDecontainersDetalhes = document.querySelectorAll('.container-detalhes');
 
 // Expande o menu lateral
@@ -15,20 +13,50 @@ btnExpandir.addEventListener('click', () => {
     btnExpandir.classList.toggle('invertido-vertical');
 });
 
-// Faz a inclusão de opacidade nos elementos que não foram clicados na lista de detalhes.
+// Ativa o menu correspondente com a seção da página
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const menuItems = document.querySelectorAll('.lista-item');
+
+    function setActiveLink() {
+        let index = sections.length;
+
+        // Verifica a posição da rolagem para definir o índice correto
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        // Remove a classe 'ativo' e o estilo de todos os itens do menu
+        menuItems.forEach((item) => {
+            item.classList.remove('ativo');
+            item.querySelector('.lista-item-link').style.color = ''; // Remove o estilo do link
+        });
+
+        // Garante que o índice está dentro do intervalo
+        if (index >= 0 && index < menuItems.length) {
+            menuItems[index].classList.add('ativo');
+            menuItems[index].querySelector('.lista-item-link').style.color = '#202020'; // Adiciona o estilo ao link ativo
+        };
+    };
+
+    setActiveLink();
+    window.addEventListener('scroll', setActiveLink);
+});
+
+
+// Faz a inclusão de opacidade nos elementos que não foram clicados na lista de detalhes
 ListaDecontainersDetalhes.forEach(container => {
     container.addEventListener('click', (event) => {
         event.stopPropagation(); // Impede que o clique se propague para o documento
         ListaDecontainersDetalhes.forEach(item => {
-            item.classList.add('container-detalhes-opacidade');
+            item.classList.toggle('container-detalhes-opacidade');
         });
         container.classList.remove('container-detalhes-opacidade');
     });
 });
 
-//
+// Ao clicar em qualquer parte do body, a classe de opacidade é retirada
 document.addEventListener('click', () => {
     ListaDecontainersDetalhes.forEach(item => {
         item.classList.remove('container-detalhes-opacidade');
     });
 });
+
